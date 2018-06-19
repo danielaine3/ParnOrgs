cbbe1523547bca2bce6cc8b3f5d2c1aa74e0cbd9
Dani Carter <carte470@d.umn.edu>
2018-06-18 21:37:54 -0500

    Header Component Added

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+	<div className="header">
+		<h4>Parnassus Preparatory School</h4>
+		<h1>Animus & Accendo Registration</h1>
+	</div>
+	);
+
+export default Header
