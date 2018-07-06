47f2cbb13497417c80d6d7d974af5f61c59d93de
Dani Carter <carte470@d.umn.edu>
2018-07-06 05:13:35 -0500

    Added days selection to signup form

--- client/src/pages/Signups/Signups.js
+++ client/src/pages/Signups/Signups.js
@@ -51,6 +51,12 @@ class Signups extends Component {
           <input label="Animus" name="Animus" type="checkbox" required value={this.state.animus} onChange={this.handleInputChange} />
           <input label="Accendo" name="Accendo" type="checkbox" required value={this.state.accendo} onChange={this.handleInputChange} />
           <br />
+          <p>Days</p>
+          <input label="Monday" name="Monday" type="checkbox" required value={this.state.monday} onChange={this.handleInputChange} />
+          <input label="Tuesday" name="Tuesday" type="checkbox" required value={this.state.Tuesday} onChange={this.handleInputChange} />
+          <input label="Wednesday" name="Wednesday" type="checkbox" required value={this.state.wednesday} onChange={this.handleInputChange} />
+          <input label="Thursday" name="Thursday" type="checkbox" required value={this.state.thursday} onChange={this.handleInputChange} />
+          <input label="Friday" name="Friday" type="checkbox" required value={this.state.Friday} onChange={this.handleInputChange} />
           <button onClick={this.submitForm}>Signup</button>
         </form>
       </div>
