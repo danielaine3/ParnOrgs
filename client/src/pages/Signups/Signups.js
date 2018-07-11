b00d1dfea4e62e2db1052e7aed3dd4c208940dc1
Dani Carter <carte470@d.umn.edu>
2018-07-10 20:03:52 -0500

    updating state

--- client/src/pages/Signups/Signups.js
+++ client/src/pages/Signups/Signups.js
@@ -53,10 +53,10 @@ class Signups extends Component {
           <br />
           <p>Days</p>
           <input label="Monday" name="Monday" type="checkbox" required value={this.state.monday} onChange={this.handleInputChange} />
-          <input label="Tuesday" name="Tuesday" type="checkbox" required value={this.state.Tuesday} onChange={this.handleInputChange} />
+          <input label="Tuesday" name="Tuesday" type="checkbox" required value={this.state.tuesday} onChange={this.handleInputChange} />
           <input label="Wednesday" name="Wednesday" type="checkbox" required value={this.state.wednesday} onChange={this.handleInputChange} />
           <input label="Thursday" name="Thursday" type="checkbox" required value={this.state.thursday} onChange={this.handleInputChange} />
-          <input label="Friday" name="Friday" type="checkbox" required value={this.state.Friday} onChange={this.handleInputChange} />
+          <input label="Friday" name="Friday" type="checkbox" required value={this.state.friday} onChange={this.handleInputChange} />
           <input label="All Week" name="allWeek" type="checkbox" require value={this.state.allWeek} onChange={this.handleInputChange} />
           <button onClick={this.submitForm}>Signup</button>
         </form>
