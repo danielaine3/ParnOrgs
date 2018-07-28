305bf0939ed8ce56286f503e24ff2bcb7ee5121b
Dani Carter <carte470@d.umn.edu>
2018-07-28 18:16:15 -0500

    sign up form stylin

--- client/src/pages/Signups/Signups.js
+++ client/src/pages/Signups/Signups.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "./Signups.css";
 
 class Signups extends Component {
     state = {
@@ -41,23 +42,32 @@ class Signups extends Component {
         <div id="formTitle">Animus/Accendo Signup</div>
         <form>
           <p>Scholar's Name:</p>
+          <br />
           <input label="First Name" placeholder="First" name="firstname" type="text" required value={this.state.childfirstname} onChange={this.handleInputChange} />
           <input label="Last Name" placeholder="Last" name="lastname" type="text" required value={this.state.childlastname} onChange={this.handleInputChange} />
           <br />
           <p>Scholar's Grade:</p>
+          <br />
           <input label="Grade" placeholder="Grade" name="Grade" type="text" required value={this.state.grade} onChange={this.handleInputChange} />
           <br />
-          <p>Program:</p>
-          <input label="Animus" name="Animus" type="checkbox" required value={this.state.animus} onChange={this.handleInputChange} /> <p>Animus</p>
-          <input label="Accendo" name="Accendo" type="checkbox" required value={this.state.accendo} onChange={this.handleInputChange} /> <p>Accendo</p>
+          <p>Scholar's Homeroom Teacher:</p>
           <br />
-          <p>Days</p>
-          <input label="Monday" name="Monday" type="checkbox" required value={this.state.monday} onChange={this.handleInputChange} /> <p>Monday</p>
-          <input label="Tuesday" name="Tuesday" type="checkbox" required value={this.state.tuesday} onChange={this.handleInputChange} /><p>Tuesday</p>
-          <input label="Wednesday" name="Wednesday" type="checkbox" required value={this.state.wednesday} onChange={this.handleInputChange} /><p>Wednesday</p>
-          <input label="Thursday" name="Thursday" type="checkbox" required value={this.state.thursday} onChange={this.handleInputChange} /><p>Thursday</p>
-          <input label="Friday" name="Friday" type="checkbox" required value={this.state.friday} onChange={this.handleInputChange}/><p>Friday</p>
-          <input label="All Week" name="allWeek" type="checkbox" require value={this.state.allWeek} onChange={this.handleInputChange} /><p>All Week</p>
+          <input label="teacher" placeholder="Homeroom teacher" name="teacher" type="text" required value={this.state.homeroom} onChange={this.handleInputChange} />
+          <br />
+          <p>Program:</p>
+          <br />
+          <p><input label="Animus" name="Animus" type="checkbox" required value={this.state.animus} onChange={this.handleInputChange} /> Animus </p>
+          <p><input label="Accendo" name="Accendo" type="checkbox" required value={this.state.accendo} onChange={this.handleInputChange} /> Accendo</p>
+          <br />
+          <p>Days:</p>
+          <br />
+          <p><input label="Monday" name="Monday" type="checkbox" required value={this.state.monday} onChange={this.handleInputChange} /> Monday </p>
+          <p><input label="Tuesday" name="Tuesday" type="checkbox" required value={this.state.tuesday} onChange={this.handleInputChange} /> Tuesday </p>
+          <p><input label="Wednesday" name="Wednesday" type="checkbox" required value={this.state.wednesday} onChange={this.handleInputChange} /> Wednesday </p>
+          <p><input label="Thursday" name="Thursday" type="checkbox" required value={this.state.thursday} onChange={this.handleInputChange} /> Thursday </p>
+          <p><input label="Friday" name="Friday" type="checkbox" required value={this.state.friday} onChange={this.handleInputChange}/> Friday </p>
+          <p><input label="All Week" name="allWeek" type="checkbox" require value={this.state.allWeek} onChange={this.handleInputChange} /> All Week</p>
+          <br />
           <button onClick={this.submitForm}>Signup</button>
         </form>
       </div>
