be7e25ac57d7a949c65521774c410e10a6f41829
Dani Carter <carte470@d.umn.edu>
2018-07-28 22:20:56 -0500

    file formatting

--- client/src/pages/Signups/Signups.js
+++ client/src/pages/Signups/Signups.js
@@ -43,75 +43,73 @@ class Signups extends Component {
     //   }
     // }
 
-    // handleInputChange = (event) => this.setState({ [event.target.name]: event.target.value });
+  handleInputChange = (event) => this.setState({ [event.target.name]: event.target.value });
 
-    // handleInputChangeForAutoCompleteField = name => (value) => {
-    //   // console.log('name, value', name, value);
-    //   const dataToSet = {};
-    //   dataToSet[name] = value;
-    //   // console.log('new state data to set', dataToSet);
-    //   this.setState(dataToSet);
-    // }
+  handleInputChangeForAutoCompleteField = name => (value) => {
+    // console.log('name, value', name, value);
+    const dataToSet = {};
+    dataToSet[name] = value;
+    // console.log('new state data to set', dataToSet);
+    this.setState(dataToSet);
+  }
   
     // submitForm = (event) => {
     //   event.preventDefault();
     //   this.handleFormSubmit();
     // }
 
-    handleFormSubmit = (event) => {
-      event.preventDefault();
-      if (this.state.childfirstname && this.state.childlastname && this.state.grade && this.state.homeroom !== "") {
-        console.log('current state', this.state);
-        console.log("COMPLETE");
-        let data = {
-          chilefirstname: this.state.childfirstname,
-          childlastname: this.state.childlastname,
-          grade: this.state.grade,
-          homeroom: this.state.homeroom,
-          monday:this.state.monday,
-          tuesday:this.state.tuesday,
-          wednesday:this.state.wednesday,
-          thursday:this.state.thursday,
-          friday:this.state.friday,
-          allweek:this.state.allweek,
-        };
-        API.addScholar(data).then((response) => {
-          console.log("Response from adding Scholar: ", response);
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (this.state.childfirstname && this.state.childlastname && this.state.grade && this.state.homeroom !== "") {
+      console.log('current state', this.state);
+      console.log("COMPLETE");
+      let data = {
+        chilefirstname: this.state.childfirstname,
+        childlastname: this.state.childlastname,
+        grade: this.state.grade,
+        homeroom: this.state.homeroom,
+        monday:this.state.monday,
+        tuesday:this.state.tuesday,
+        wednesday:this.state.wednesday,
+        thursday:this.state.thursday,
+        friday:this.state.friday,
+        allweek:this.state.allweek,
+      };
+      API.addScholar(data).then((response) => {
+        console.log("Response from adding Scholar: ", response);
+        this.setState({
+          childfirstname: '',
+          childlastname: '',
+          grade: '',
+          homeroom: '',
+          monday:false,
+          tuesday:false,
+          wednesday:false,
+          thursday: false,
+          friday:false,
+          allweek:false,
+        })
+        .catch((err) => {
+            console.log('Error while adding scholar: ', err);
+            this.setState({ error: "Error while adding scholar."});
+            //launch error dialog
+            this.errDialogOpen();
+            console.error(this.setSate.error, err);
+        })
+        API.getScholar().then((response) => {
           this.setState({
-            childfirstname: '',
-            childlastname: '',
-            grade: '',
-            homeroom: '',
-            monday:false,
-            tuesday:false,
-            wednesday:false,
-            thursday: false,
-            friday:false,
-            allweek:false,
-          })
-          .catch((err) => {
-              console.log('Error while adding scholar: ', err);
-              this.setState({ error: "Error while adding scholar."});
-              //launch error dialog
-              this.errDialogOpen();
-              console.error(this.setSate.error, err);
-          })
-          API.getScholar().then((response) => {
-            this.setState({
-              scholarData:response.data
-            });
-          });  
-        });
-      } else {
-      console.log("Unable to add scholar.")
-      this.setState({ error: "Incomplete data entered. Scholar require a first name, last name, grade and homeroom teacher to be added."});
-        //launch error dialog
-        this.errDialogOpen();
-      }
+            scholarData:response.data
+          });
+        });  
+      });
+    } else {
+    console.log("Unable to add scholar.")
+    this.setState({ error: "Incomplete data entered. Scholar require a first name, last name, grade and homeroom teacher to be added."});
+      //launch error dialog
+      this.errDialogOpen();
     }
-  
-  
-    
+  }
+
   render() {
     // console.log('state upon rendering: ', this.state);
     return (
@@ -141,7 +139,7 @@ class Signups extends Component {
           <p><input label="All Week" name="allWeek" type="checkbox" required value={this.state.allWeek} onChange={this.handleInputChange} /> All Week</p>
           <br />
           <button onClick={this.handleFormSubmit} 
-          // {...this.state}
+          {...this.state}
           >Signup</button>
         </form>
       </div>
