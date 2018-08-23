172d92fe9ca9598d080aa1e6415cac9afbadfe0f
Dani Carter <carte470@d.umn.edu>
2018-08-22 22:00:49 -0500

    added select box to scholar card

--- client/src/pages/ScholarInfo/ScholarCard.js
+++ client/src/pages/ScholarInfo/ScholarCard.js
@@ -4,6 +4,7 @@ import ScholarContent from './ScholarContent';
 const ScholarCard = props => (
   <div className={props.scholars}>
     <title>Scholars</title>
+    <p><input label="All Week" name="allWeek" type="checkbox" required value={props.scholar} /> Select</p>
     {props.scholars.map(scholar =>
       <ScholarContent key={scholar._id}
        {...scholar} 
