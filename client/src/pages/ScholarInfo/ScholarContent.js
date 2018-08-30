00d1a555d1c8f492d34beb7b575f4ee2a96e86ea
Dani Carter <carte470@d.umn.edu>
2018-08-29 21:39:35 -0500

    select data from card work

--- client/src/pages/ScholarInfo/ScholarContent.js
+++ client/src/pages/ScholarInfo/ScholarContent.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ScholarContent = props => (
     <div className={props.card}>
         <div className="cardcontent">
-            <p><input label="All Week" name="allWeek" type="checkbox" required value={props.scholar} /> </p>
+            <p><input label="select" name="select" type="checkbox" required value={props.scholar} /> </p>
             <p class= "props">{props.childfirstname}</p>
             <p class= "props">{props.childlastname}</p>
             <p class= "props">{props.grade}</p>
